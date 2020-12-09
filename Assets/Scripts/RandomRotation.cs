19210f768846fbafa93a235901d06d72ae7568dc
Daniel Mendelowitz <dan@24bitgames.com>
2020-12-09 12:19:23 +0200

    Random rotation is better now

--- Assets/Scripts/RandomRotation.cs
+++ Assets/Scripts/RandomRotation.cs
@@ -1,14 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class RandomRotation : MonoBehaviour
 {
-	[SerializeField] private float rotationSpeed = 0.25f;
-	[SerializeField] private Vector3 rotationEuler;
+    [SerializeField] private float rotationSpeed = 0.25f;
+    [SerializeField] private float randomRange = 10f;
+    [SerializeField] private Vector3 rotationEuler;
 
-	void Update()
-	{
-		transform.Rotate(rotationEuler * rotationSpeed, Space.Self);
-	}
+    private void Start()
+    {
+        float randomX = Random.Range(-randomRange, randomRange);
+        float randomY = Random.Range(-randomRange, randomRange);
+        float randomZ = Random.Range(-randomRange, randomRange);
+
+        rotationEuler = new Vector3(randomX, randomY, randomZ);
+    }
+
+    void Update()
+    {
+        transform.Rotate(rotationEuler * rotationSpeed, Space.Self);
+    }
 }
--- Assets/Scripts/SimpleDragger.cs
+++ Assets/Scripts/SimpleDragger.cs
@@ -5,26 +5,13 @@ using UnityEngine.EventSystems;
 
 public class SimpleDragger : MonoBehaviour
 {
-	private float currentZ;
-	private Vector3 worldMousePos;
-	private void Start()
-	{
-		currentZ = transform.position.z;
-	}
-	private void OnMouseDown()
-	{
-		Debug.Log($"{gameObject.name} mouse down.");
-	}
-	private void OnMouseDrag()
-	{
-		Debug.Log($"{gameObject.name} mouse drag.");
-		worldMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-		transform.position = new Vector3(worldMousePos.x, worldMousePos.y, currentZ);
-	}
+    [SerializeField] private Camera screenCamera;
+    private float currentZ;
+    private Vector3 worldMousePos;
+    private Vector2 screenMousePos;
 
-	private void OnMouseUp()
-	{
-		Debug.Log($"{gameObject.name} mouse up.");
-
-	}
+    private void Start()
+    {
+        currentZ = transform.position.z;
+    }
 }
